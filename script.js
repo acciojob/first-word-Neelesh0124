8afd69f41c9ad1ca459f5d8e0cc5d9a07fd70371
Neelesh0124 <142391511+Neelesh0124@users.noreply.github.com>
2024-03-29 10:27:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,23 @@
 function firstWord(s) {
   // your code here
+	let i = 0;
+	while (s[i]==' ') {
+		s.length = s.length-1;	
+	}
+	for (let i = 0; i < s.length; i++) {
+		if (s[i]==' ') {
+			break;
+			
+		}
+		else{
+			console.log(s[i])
+		}
+		
+	}
+	
 }
 
 // Do not change the code below
 
-const s = prompt("Enter String:");
-alert(firstWord(s));
+//const s = prompt("Enter String:");
+//alert(firstWord(s));
